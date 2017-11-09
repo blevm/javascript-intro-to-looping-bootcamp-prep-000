991181bbe2ba117b8571030ab0679d6721ef796e
Michelle B <mjblevins7@gmail.com>
2017-11-09 13:20:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -26,9 +26,8 @@ function doWhileLoop(array) {
   return Math.random() >= 0.5
   }
 
-  do array.slice(1);
-  }
-  
-  while (array.length > 0 && maybeTrue());
+  do { array.slice(1);
+  } while (array.length > 0 && maybeTrue());
+
   return array;
 }
